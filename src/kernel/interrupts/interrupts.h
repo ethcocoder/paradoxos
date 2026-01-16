4e25c07fc8488a0eef814e91a3d61868333801a7
ethcocoder <natnaelermiyas461@gmail.com>
2026-01-17 01:10:01 +0300

    new

--- src/kernel/interrupts/interrupts.h
+++
@@ -1,9 +0,0 @@
-
-#include <devices/cpu/cpu.h>
-
-#ifndef INTERRUPTS_H
-#define INTERRUPTS_H
-
-#define SPURIOUS_INTERRUPT 255
-
-#endif
--- src/kernel/logo.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOGO_H
-#define LOGO_H
-
-extern const char *paradox_logo_text;
-
-#endif
